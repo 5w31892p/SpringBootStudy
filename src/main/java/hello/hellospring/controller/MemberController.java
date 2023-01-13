bcfcb318701d2cca840e581ff1f259ea45d182cc
Seowoo <5w31892p@gmail.com>
2023-01-13 19:24:17 +0900

    feat : Member create function add

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,7 +2,10 @@ package hello.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -13,5 +16,19 @@ public class MemberController {
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
+	}
+
+	@GetMapping(value = "/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping(value = "/members/new")
+	public String create(MemberForm memberForm) {
+		Member member = new Member();
+		member.setName(memberForm.getName());
+		memberService.join(member);
+
+		return "redirect:/";
 	}
 }
